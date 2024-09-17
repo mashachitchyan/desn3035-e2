b6034b4981b2535a661b9c007e75c07160b909a9
mashachitchyan <margarita.chitchyan@gmail.com>
2024-09-17 03:34:20 +0000

    Added bootstrap

--- app/content.tsx
+++ app/content.tsx
@@ -1,10 +1,25 @@
 "use client"
 
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css"
 
 export default function Content() {
 
     return <>
-        <h1>Hello, Next.js!</h1>
+        <div className="container">
+
+            <h1>My first day at work!</h1>
+            <div className="row">
+                <div className="col-6">
+                    [MAP GOES HERE]
+                </div>
+                <div className="col-6">
+                    [CHART GOES HERE]
+                </div>
+                <div className="col-12">
+                    [PLAYER GOES HERE]
+                </div>
+            </div>
+        </div>
     </>
 }
