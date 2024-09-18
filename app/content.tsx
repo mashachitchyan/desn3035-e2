4150295c446583500bda00a592aee5e045478033
mashachitchyan <margarita.chitchyan@gmail.com>
2024-09-18 00:05:59 +0000

    Added map, chart and player

--- app/content.tsx
+++ app/content.tsx
@@ -1,7 +1,56 @@
 "use client"
 
 import React from "react";
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.css";
+import ReactPlayer from 'react-player';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
+
+const data = [
+    {
+        name: 'Page A',
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'Page B',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'Page C',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'Page D',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'Page E',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'Page F',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'Page G',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
 
 export default function Content() {
 
@@ -10,14 +59,44 @@ export default function Content() {
 
             <h1>My first day at work!</h1>
             <div className="row">
+            <div className="col-6">
+                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{height: 500}}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
                 <div className="col-6">
-                    [MAP GOES HERE]
-                </div>
-                <div className="col-6">
-                    [CHART GOES HERE]
+                    <ResponsiveContainer width="100%" minHeight="500px">
+                        <LineChart
+                            width={500}
+                            height={300}
+                            data={data}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                        </LineChart>
+                    </ResponsiveContainer>
                 </div>
                 <div className="col-12">
-                    [PLAYER GOES HERE]
+                    <ReactPlayer width="100%" url='https://www.youtube.com/watch?v=LXb3EKWsInQ' />
                 </div>
             </div>
         </div>
